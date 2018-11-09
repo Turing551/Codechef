0cd9bfe9cdf00395cb6c377c44ad8ca4fe7a8236
K N V ASHOK <37957322+Turing551@users.noreply.github.com>
2018-11-09 18:51:52 +0530

    Create CARDMGK.cpp

---
+++ Snackdown 2019/1A/CARDMGK.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int x,y,arr[100100];
+int main(){
+    cin>>x;
+    for(int i=1;i<=x;i++){
+        cin>>y;
+        for(int j=1;j<=y;j++)
+            cin>>arr[j];
+        int j;
+        for(j=2;j<=y;j++){
+            if(arr[j-1]>arr[j])
+                break;
+        }
+        if(j<y){
+            int k;
+            for(k=j;k<=y;k++){
+                if(arr[k-1]>arr[k] && j!=k){
+                    break;
+                } }
+            if(arr[y]<=arr[1] && k==y+1){
+                cout<<"YES";
+            }
+            else{
+                cout<<"NO";
+            } }
+        else{
+            cout<<"YES";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
