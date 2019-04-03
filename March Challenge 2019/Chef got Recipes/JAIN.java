e89c1f9493b91fd26fb34122aeeb296d7b992f20
K N V ASHOK <meeturfrndashok8@gmail.com>
2019-04-03 15:18:22 +0530

    Hurray Accepted 100pts

--- March Challenge 2019/Chef got Recipes/JAIN.java
+++ March Challenge 2019/Chef got Recipes/JAIN.java
@@ -1,35 +1,44 @@
-import java.util.*;
+import java.io.*;
 
-class Solution{
-	public static void main(String args[]){	
-		Scanner sc=new Scanner(System.in);
-		int a=sc.nextInt();
-		int b=0;	
-		while(a>b){	
-			int n=sc.nextInt();
-			String str[]=new String[n];	
-			HashSet<Character> hs[]=new HashSet[n];
-			for(int i=0;i<n;i++){	
-				str[i]=sc.next();
-				hs[i]=new HashSet<>();	
-				for(char c : str[i].toCharArray()){
-					if(hs[i].contains(c)==false){
-						hs[i].add(c);
-					} } }		
-			int count=0;
-			Character x[]={'a','e','i','o','u'};
-			HashSet<Character> y=new HashSet<>(Arrays.asList(x));			
-			for(int i=0;i<n;i++){
-				for(int j=i+1;j<n;j++){
-					boolean k=true;					
-					for(char c : y){
-						if(hs[i].contains(c) == false && hs[j].contains(c) == false){
-							k=false;
-							break;
-						} }					
-					if(k==true){
-						count ++;
-					} } }		
-			System.out.println(count);
-			b++;
-		} } }
+class Solution{ 
+  public static void main(String args[]) throws IOException{
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    String str=br.readLine();
+    int cnt=Integer.parseInt(str);  
+    for(int i=0;i<cnt;i++){
+      int arr[][]=new int[6][32];
+      str=br.readLine();
+      int n=Integer.parseInt(str);    
+      for(int j=0;j<n;j++){
+        str=br.readLine();     
+        int k=0;
+        if(str.indexOf("u")!=-1) k+=1;
+        if(str.indexOf("o")!=-1) k+=2;
+        if(str.indexOf("i")!=-1) k+=4;
+        if(str.indexOf("e")!=-1) k+=8;
+        if(str.indexOf("a")!=-1) k+=16;      
+        int c=Integer.bitCount(k);
+        arr[c][k]++;
+      }
+      long answer=0;
+      for(int x=1;x<5;x++){
+        for(int y=x;y<=5;y++){
+          long a=0;
+          for(int p=1;p<32;p++){
+            for(int q=1;q<32;q++){
+              if((p|q)==31){
+                  a=a+(arr[x][p]*arr[y][q]);
+              } } }      
+          if(x==y){
+            long b=a/2;
+            answer=answer+b;
+          }else{
+            answer=answer+a;
+          } } }
+      int temp=arr[5][31];
+      answer=answer+(((temp-1)*temp)/2);   
+      System.out.println(answer);
+    }  
+    br.close();  
+  }
+}
