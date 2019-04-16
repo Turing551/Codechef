0a2947b3182d07d0309e1bbf4a972c8f503e02e4
K N V ASHOK <meeturfrndashok8@gmail.com>
2019-04-16 18:06:04 +0530

    Accepted..

--- April Challenge 2019/Friend or Girlfriend/STRCH.py
+++ April Challenge 2019/Friend or Girlfriend/STRCH.py
@@ -1,1 +1,16 @@
-Solution will be added after the contest ends.
+while True:
+    try:
+        n=int(input())
+        for i in range(n):
+            k=int(input())
+            p,q=input().strip().split()
+            x,y=[],0
+            for j in range(len(p)):
+                if p[j]!=q:
+                    x.append(y)
+                else:
+                    y=j+1
+                    x.append(y)
+            print(sum(x))
+    except:
+        break
