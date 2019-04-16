ad6521f5f8a028ddf680000ae74d650cd154a7f6
K N V ASHOK <meeturfrndashok8@gmail.com>
2019-04-16 18:02:45 +0530

    Accepted..

--- April Challenge 2019/Maximum Remaining/MAXREM.py
+++ April Challenge 2019/Maximum Remaining/MAXREM.py
@@ -1,1 +1,10 @@
-Solution will be added after the contest ends.
+n=int(input())
+x=0
+arr=list(map(int,input().split()))
+arr.sort(reverse=True)
+y=arr[0]
+for i in range(1,n):
+    if y!=arr[i]:
+        x=arr[i]
+        break
+print(x)
