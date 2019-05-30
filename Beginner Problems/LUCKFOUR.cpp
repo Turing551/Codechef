b192c9a7955b81efb35eecc007517daefa12a2c2
K N V ASHOK <meeturfrndashok8@gmail.com>
2019-05-30 11:53:42 +0530

    Accepted

---
+++ Beginner Problems/LUCKFOUR.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <climits>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+#define ll long long
+
+#define MIN(a, b) a < b ? a : b
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int t;
+    scanf("%d", &t);
+
+    while( t-- ) {
+        int answer = 0;
+        char str[1024];
+        scanf("%s", str);
+        for( int i = 0 ; str[i]; i++ ) {
+            answer += (str[i] == '4');
+        }
+        printf("%d\n", answer);
+    }
+    return 0;
+}
