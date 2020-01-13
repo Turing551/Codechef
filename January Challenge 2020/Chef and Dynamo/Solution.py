7bc0e7a606b235301bfdfcecca7a7ef03fa1a97f
K N V ASHOK <meeturfrndashok8@gmail.com>
2020-01-13 16:53:29 +0530

    Accepted

---
+++ January Challenge 2020/Chef and Dynamo/Solution.py
@@ -0,0 +1,16 @@
+for i in range(int(input())):
+    n=int(input())
+    n1=int(input())
+    x=(((10**n)*2)+n1)
+    print(x)
+    n2=int(input())
+    y=(((10**n)-1)-n2)+1
+    print(y)
+    n3=int(input())
+    z=(((10**n)-1)-n3)+1
+    print(z)
+    res=int(input())
+    if res==1:
+        pass
+    else:
+        exit(1)
